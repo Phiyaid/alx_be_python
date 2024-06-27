3b536b1327682bd5b7a9a1f7da3bc74dcd34ef45
Philip Yao Aidam <phlpaidam@gmail.com>
2024-06-27 20:10:35 +0000

    Add script to multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,5 +1,5 @@
-number = int(input("Enter a number to see its multiplication tables: "))
+number = input("Enter a number to see its multiplication tables: ")
 
 for item in range(1, 11):
-    product = number * item
+    product = int(number) * item
     print(f"{number} * {item} = {product}")
