214b88b20befc3f6113e9393c386d0467d7e90d1
Phiyaid <phlpaidam@gmail.com>
2024-06-28 13:03:17 +0000

    Delete a line

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,5 +1,4 @@
 number = int(input("Enter a number to see its multiplication tables: "))
 
 for item in range(1, 11):
-    product = int(number) * item
-    print(f"{number} * {item} = {product}")
+    print(f"{number} * {item} = {product * item}")
