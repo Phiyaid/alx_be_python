8de044c4335f838e42a3adf2accb56b9480a29fb
Phiyaid <phlpaidam@gmail.com>
2024-07-06 16:17:17 +0000

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,18 @@
 def perform_operation(num1, num2, operation):
     
-    try:
-        if operation == "add":
-            return num1 + num2
-        elif operation == "subtract":
-            return num1 - num2
-        elif operation == "multiply":
-            return num1 * num2
-        elif operation == "divide": 
+
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide": 
+        if num2 == 0:
+            print("You can't divide by 0.")
+            return 1
+        else: 
             return num1 / num2
-        else:
+    else:
             print("Enter a valid operation")
-    except ZeroDivisionError : return 1
+    
