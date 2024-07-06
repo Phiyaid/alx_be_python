d7f3ce1a186432d90605adbb1e609686431e30eb
Phiyaid <phlpaidam@gmail.com>
2024-07-06 15:54:55 +0000

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,9 +1,9 @@
 def perform_operation(num1, num2, operation):
     
-try:
-    match operation:
-        case "add": return chosen = num1 + num2
-        case "subtract": return chosen = num1 - num2
-        case "multiply": return chosen = num1 * num2
-        case "divide": return chosen = num1 / num2
-except ZeroDivisionError : return 1
+    try:
+        match operation:
+            case "add": return chosen = num1 + num2
+            case "subtract": return chosen = num1 - num2
+            case "multiply": return chosen = num1 * num2
+            case "divide": return chosen = num1 / num2
+    except ZeroDivisionError : return 1
