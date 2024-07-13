cb36698e15314aad9520f345ec04648d022ddbbb
Phiyaid <phlpaidam@gmail.com>
2024-07-13 19:35:50 +0000

    Add bank_account.py and main-0.py

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,19 @@
+class BankAccount:
+    def __init__(self, account_balance = None):
+        self.account_balance = self.account_balance = account_balance
+        
+        
+        
+    def deposit(self,amount):
+        self.amount = amount + self.account_balance
+        return self.amount
+    
+    def withdraw(self, amount):
+        if amount > self.account_balance:
+            self.account_balance = self.account_balance - amount 
+            return True
+        else:
+            return False
+        
+    def display_balance(self):
+        print(f"Current Balance: ${self.account_balance}")
