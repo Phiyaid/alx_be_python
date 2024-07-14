a39a7add5bcb1cad63eee5f7d928e89efa078c6e
Phiyaid <phlpaidam@gmail.com>
2024-07-14 05:06:24 +0000

    Add SimpleCalculator class and unit tests

---
+++ test_simple_calculator.py
@@ -0,0 +1,33 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+    def setUp(self):
+        """Set up the SimpleCalculator instance before each test."""
+        self.calc = SimpleCalculator()
+
+    def test_addition(self):
+        """Test the addition method."""
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        
+    def test_subtract(self):
+        """Test the subtraction method."""
+        self.assertEqual(self.calc.subtract(0, 0), 0)
+        self.assertEqual(self.calc.subtract(-1, 1), -2)
+        
+    def test_multiply(self):
+        """Test the multiplication method."""
+        self.assertEqual(self.calc.multiply(0, 0), 0)
+        self.assertEqual(self.calc.multiply(-1, 1), -1)
+        self.assertEqual(self.calc.multiply(-1, -1), 1)
+        self.assertEqual(self.calc.multiply(0, 6), 0)
+        
+    def test_division(self):
+        """Test the division method."""
+        self.assertEqual(self.calc.divide(5, 1), 5)
+        self.assertEqual(self.calc.divide(0, 1),0)
+        with self.assertRaises(ZeroDivisionError):
+            self.calc.divide(1, 0)
+            
+            
